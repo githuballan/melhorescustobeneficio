19c7ee2a7c8b7a3f776a9b4f83983aa6f0aac7b3
Allan <162851663+githuballan@users.noreply.github.com>
2025-04-30 14:36:21 -0300

    perfumaria dias das maes

--- ofertas-do-dia/script-ofertas.js
+++ ofertas-do-dia/script-ofertas.js
@@ -48,6 +48,18 @@ document.addEventListener('DOMContentLoaded', () => {
         const produtos = [
 
 
+
+
+
+
+
+
+//---------------------STANLEY---------------------//
+//---------------------STANLEY---------------------//
+          //ACABA DIA DAS MAES
+          //ACABA DIA DAS MAES
+
+
               new ProdutoOferta(
               'MUG',//Titulo do produto aqui',
               'Camp Mug Stanley | 350ML',//Descrição do produto aqui',
@@ -190,6 +202,29 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+
+
+
+
+
+
+
+
+          //ACABA DIA DAS MAES
+          //ACABA DIA DAS MAES
+//---------------------STANLEY---------------------//
+//---------------------STANLEY---------------------//
+
+
+
+
+
+
+
+//---------------------MAES---------------------//
+//---------------------MAES---------------------//
+
+
             new ProdutoOferta(
               'Celular',//Titulo do produto aqui',
               'Smartphone Samsung Galaxy A15 6,5" 128GB Azul Escuro 4G 4GB RAM Câm. Tripla 50MP + Selfie 13MP 5000mAh Dual Chip',//Descrição do produto aqui', amazon magalu
@@ -238,6 +273,81 @@ document.addEventListener('DOMContentLoaded', () => {
            ),
 
 
+             new ProdutoOferta(
+              'La Vie Est Belle Lancôme',//Titulo do produto aqui',
+              'Perfume Feminino La Vie Est Belle Lancôme EDP 100ml',//Descrição do produto aqui', amazon mercadolivre magalu
+              'mercadolivre',//Loja do produto aqui',
+              '/imagens/ofertas-do-dia/maes/lacome.webp',//src da imagem aqui',
+              'Imagem copo Stanley',//alt da imagem aqui',
+              'https://www.mercadolivre.com.br/perfume-feminino-la-vie-est-belle-lancme-edp-100ml/p/MLB6140723?pdp_filters=deal%3AMLB779362-1#polycard_client=homes-korribanSearchPromotions&searchVariation=MLB6140723&wid=MLB5011962698&position=8&search_layout=grid&type=product&tracking_id=2ceac590-456a-484b-bd3e-f9b0b3694c7c&sid=search&c_id=/home/promotions-recommendations/element&c_uid=f21682d1-b63b-4b45-b0b9-4d7f0eb39b40',//url do produto aqui',
+              459 ,//valor do produto aqui',
+            'perfumaria',//categoria do produto aqui' tecnologia variados fitness
+           ),
+
+
+           new ProdutoOferta(
+            'Una Blush Deo Parfum',//Titulo do produto aqui',
+            'Una Blush Deo Parfum Feminino 75ml Natura',//Descrição do produto aqui', amazon mercadolivre magalu
+            'mercadolivre',//Loja do produto aqui',
+            '/imagens/ofertas-do-dia/maes/una.webp',//src da imagem aqui',
+            'Imagem copo Stanley',//alt da imagem aqui',
+            'https://www.mercadolivre.com.br/social/melhorescustobenefic?matt_tool=58695067&forceInApp=true&ref=BCUXZ%2B1vdmS0Iy0eXryliK5IALl9vw%2FxdBXD1FZr8QCqwnn%2BQ3SWlxNU%2FRsb4FZrokh7a2%2BMgNBWFE4yyi4yturVoWbqQLqv%2B%2BLGqVucbLAAkavP3rrrBJugr0bGUA1pqwl%2BR9rk7OV%2F52UGRuJIUwZatfuYetaQPpcQYr1w0Nj7TmK5kVFQ8Iz51olIMA4XLR%2FQfW4%3D',//url do produto aqui',
+            169 ,//valor do produto aqui',
+          'perfumaria',//categoria do produto aqui' tecnologia variados fitness
+         ),
+
+
+         new ProdutoOferta(
+          'Fakhar Lattafa Rose',//Titulo do produto aqui',
+          'Perfume Fakhar Lattafa Rose feminino 100ml',//Descrição do produto aqui', amazon mercadolivre magalu
+          'mercadolivre',//Loja do produto aqui',
+          '/imagens/ofertas-do-dia/maes/fakhar.webp',//src da imagem aqui',
+          'Imagem do perfume',//alt da imagem aqui',
+          'https://www.mercadolivre.com.br/social/melhorescustobenefic?matt_tool=58695067&forceInApp=true&ref=BJrbIHfZSLt9dKZ0TBzA1oDsw4qqrXVhkfWw%2FsiMNSOvaltXoiNGUaPEXF7ManClrrvaiUm%2BzHyBNhIToQ1ZB1oi45kUm6Hx1moAZfD16dKGSlJ9jY6KV%2B5V6tSGSfACiIbxEpZBv8RdaFqFV%2FwaqR2nJyOov686Htyn%2FBAk7tNRMsLVrvZKX%2Bt2CPnjeQOs3VIo%2FwE%3D',//url do produto aqui',
+          170 ,//valor do produto aqui',
+        'perfumaria',//categoria do produto aqui' tecnologia variados fitness
+       ),
+
+              
+        new ProdutoOferta(
+          'Lattafa Yara',//Titulo do produto aqui',
+          'Perfume Feminino Lattafa Yara eau de parfum 100 ml',//Descrição do produto aqui', amazon mercadolivre magalu
+          'mercadolivre',//Loja do produto aqui',
+          '/imagens/ofertas-do-dia/maes/latafa.webp',//src da imagem aqui',
+          'Imagem do perfume',//alt da imagem aqui',
+          'https://www.mercadolivre.com.br/social/melhorescustobenefic?matt_tool=58695067&forceInApp=true&ref=BM7ACFISk1uZTUMeGOJ0G%2FruZlV2rXMx1yRV0SZcWDteB21hcWo6F4pjZwXNx8JzV2bTq%2BuXjRDx9ppmPbHse%2Fz0ZZvPz3XIkhcYUFSNM%2B8ft1eIPEiW0aytBL2UC3q6BdpLMQyDHDDMTUh2Hgekcmt6%2BBOTngyyYxq9QLW%2BysiCslVwjGxpdyDBywa5Wb9KZ9mAKsE%3D',//url do produto aqui',
+          183 ,//valor do produto aqui',
+        'perfumaria',//categoria do produto aqui' tecnologia variados fitness
+        ),
+
+                
+        new ProdutoOferta(
+          'Lancôme Idôle',//Titulo do produto aqui',
+          'Perfume Feminino Lancôme Idôle EDP 100ml',//Descrição do produto aqui', amazon mercadolivre magalu
+          'mercadolivre',//Loja do produto aqui',
+          '/imagens/ofertas-do-dia/maes/idole.webp',//src da imagem aqui',
+          'Imagem do perfume',//alt da imagem aqui',
+          'https://www.mercadolivre.com.br/social/melhorescustobenefic?matt_tool=58695067&forceInApp=true&ref=BEXznsoFf3XxWkFYskEVuyqTICMjDa3F22w0kO0eA7hJtHXFaPh4vTPr7fbQqombnrHd5zMAvqoYDimRFJe2utDPwg2e2O337hmWkYX3M%2FGhTL0kQRSqEA0Pq0cTmQbraCUIEVhuY8v%2BV%2FGXqflxkJpCDznb78tQxZnR4KfGOswj03tnlbTmCYFcVItnhLsb%2Bj8Z%2Bpg%3D',//url do produto aqui',
+          736.82 ,//valor do produto aqui',
+        'perfumaria',//categoria do produto aqui' tecnologia variados fitness
+        ),
+
+
+
+//---------------------MAES---------------------//
+//---------------------MAES---------------------//
+
+
+// new ProdutoOferta(
+//   'XXXXXXXX',//Titulo do produto aqui',
+//   'XXXXXXXX',//Descrição do produto aqui', amazon mercadolivre magalu
+//   'mercadolivre',//Loja do produto aqui',
+//   '/imagens/ofertas-do-dia/maes/XXXXXXXX.webp',//src da imagem aqui',
+//   'Imagem do perfume',//alt da imagem aqui',
+//   'XXXXXXXX',//url do produto aqui',
+//   0000000 ,//valor do produto aqui',
+// 'perfumaria',//categoria do produto aqui' tecnologia variados fitness
+// ),
 
 
            // new ProdutoOferta(
